2816b96ffe17d56d407839165dc59a8828507640
John Champion <jtchampi@ncsu.edu>
2022-08-01 13:03:47 -0400

    Check db for permission to read or write authorizations to users.

--- controllers/authorization.py
+++ controllers/authorization.py
@@ -16,14 +16,35 @@ class UpdateAuthorizationRequestBody(BaseModel):
 
 
 @router.get('/authorizations', tags=['Authorization'])
-def get_accounts_with_authorization(app_id: str, db_filter: str, value: str):
+def get_accounts_with_authorization(response: Response,
+                                    app_id: str,
+                                    db_filter: str,
+                                    value: str,
+                                    authorization: str = Header(default=None)):
     """Gets all accounts with specified authorizations.
 
     It may be necessary to query all accounts with a certain
     authorization. This endpoint can query accounts and return that
     list of accounts.
     """
-    accounts = Account.find_by_authorization(app_id, db_filter, value)
+
+    # Get read permissions of the requesting account.
+    requesting_account = Token.decode_token(authorization.split(' ')[1])
+    requesting_account = Account.find_by_email(requesting_account['email'])
+    read_permissions: dict = requesting_account.authorizations.get(
+                                            app_id, {}).get('_read', {})
+
+    # If requesting user has permission, return accounts data.
+    if read_permissions.get(
+        'superuser', False) is True or read_permissions.get(
+            db_filter, False) is True:
+        accounts = Account.find_by_authorization(app_id, db_filter, value)
+    else:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            'error': """This account is not authorized to write to this
+            user's authorization(s)."""
+        }
 
     return {
         'accounts': accounts
@@ -31,7 +52,8 @@ def get_accounts_with_authorization(app_id: str, db_filter: str, value: str):
 
 
 @router.put('/update-authorization', tags=['Authorization'])
-def update_authorization(body: UpdateAuthorizationRequestBody,
+def update_authorization(response: Response,
+                         body: UpdateAuthorizationRequestBody,
                          authorization: str = Header(default=None)):
     """Updates the authorization for a user for a given app.
 
@@ -41,7 +63,22 @@ def update_authorization(body: UpdateAuthorizationRequestBody,
     record will be created. If a record already exists, it will be
     replaced with the new data.
     """
-    account = Account.find_by_email(body.email)
+
+    # Get write permissions of the requesting account.
+    requesting_account = Token.decode_token(authorization.split(' ')[1])
+    requesting_account = Account.find_by_email(requesting_account['email'])
+    write_permissions: dict = requesting_account.authorizations.get(
+                                            body.app_id, {}).get('_write', {})
+
+    # If requesting user has permission, write new permission(s) to the database.
+    if write_permissions.get('superuser', False) is True:
+        account = Account.find_by_email(body.email)
+    else:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            'error': """This account is not authorized to write to this
+            user's authorization(s)."""
+        }
 
     account.update_authorization(body.app_id, body.authorization)
     account.update()
@@ -61,7 +98,21 @@ def remove_authorization(response: Response,
     Takes a value for app_id, then removes the entire record of
     authorization for that app in the user's account.
     """
-    account = Account.find_by_email(email)
+
+    # Get write permissions of the requesting account.
+    requesting_account = Token.decode_token(authorization.split(' ')[1])
+    requesting_account = Account.find_by_email(requesting_account['email'])
+    write_permissions: dict = requesting_account.authorizations.get(
+                                            app_id, {}).get('_write', {})
+
+    if write_permissions.get('superuser', False) is True:
+        account = Account.find_by_email(email)
+    else:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            'error': """This account is not authorized to delete
+            this user's authorization(s)."""
+        }
 
     try:
         account.remove_authorization(app_id)
--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,3 +1,5 @@
+"""Tests for the authorization controller functions."""
+
 from fastapi.testclient import TestClient
 from main import app
 from models.Account import Account
@@ -5,23 +7,118 @@ from models.Token import Token
 
 client = TestClient(app)
 
-EMAIL = 'user@ncsu.edu'
-CAMPUS_ID = '200101234'
+ADMIN_ACCOUNT = {
+    'email': 'admin@ncsu.edu',
+    'campus_id': '00101234',
+    'authorizations': {
+        'test-app': {
+            '_read': {
+                'superuser': True
+            },
+            '_write': {
+                'superuser': True
+            }
+        }
+    }
+}
+
+ADMIN_ACCOUNT_WITHOUT_PERMISSION = {
+    'email': 'admin@ncsu.edu',
+    'campus_id': '00101234',
+    'authorizations': {
+        'test-app': {}
+    }
+}
+
+ACCOUNT_TO_BE_CHANGED = {
+    'email': 'user@ncsu.edu',
+    'campus_id': '200101234',
+    'authorizations': {}
+}
+
+
+def test_get_authorizations(monkeypatch):
+    """It should get a user's authorizations given a query."""
+
+    def mock_find_by_email(email):
+        """Mock Account.find_by_email function."""
+        if email == 'admin@ncsu.edu':
+            return Account(ADMIN_ACCOUNT)
+        else:
+            return Account(ACCOUNT_TO_BE_CHANGED)
+
+    monkeypatch.setattr(Account, "find_by_email", mock_find_by_email)
+
+    token = Token.generate_token(ADMIN_ACCOUNT)
+    response = client.get(
+        '/authorizations?app_id=test-app&db_filter=access&value=True',
+                          headers={'Authorization': f'Bearer {token}'},
+                          json={
+                              'email': 'user@ncsu.edu',
+                              'app_id': 'test-app',
+                              'authorization': {'access': True}
+                          })
+
+    assert response.status_code == 200
+    assert response.json() == {
+        'accounts': []
+    }
+
+
+def test_get_authorizations_without_permission(monkeypatch):
+    """
+    It should fail to get a user's authorizations if the requester
+    does not have permission.
+    """
+
+    def mock_find_by_email(email):
+        """Mock Account.find_by_email function."""
+        if email == 'admin@ncsu.edu':
+            return Account(ADMIN_ACCOUNT_WITHOUT_PERMISSION)
+        else:
+            return Account(ACCOUNT_TO_BE_CHANGED)
+
+    monkeypatch.setattr(Account, "find_by_email", mock_find_by_email)
+
+    token = Token.generate_token(ADMIN_ACCOUNT)
+    response = client.get(
+        '/authorizations?app_id=test-app&db_filter=access&value=True',
+                          headers={'Authorization': f'Bearer {token}'},
+                          json={
+                              'email': 'user@ncsu.edu',
+                              'app_id': 'test-app',
+                              'authorization': {'access': True}
+                          })
+
+    assert response.status_code == 400
 
 
 def test_update_authorization(monkeypatch):
-    def mock_find_by_email(*args, **kwargs):
-        return Account({'id': '62bc911d2947f5aa76600598', 'email': EMAIL, 'campus_id': CAMPUS_ID, 'authorizations': {}})
+    """It should ensure a user's authorizations can be updated."""
 
-    def mock_update(*args, **kwargs):
+    def mock_find_by_email(email):
+        """Mock Account.find_by_email function."""
+        if email == 'admin@ncsu.edu':
+            return Account(ADMIN_ACCOUNT)
+        else:
+            return Account(ACCOUNT_TO_BE_CHANGED)
+
+    def mock_update(*_, **__):
+        """Mock Account.update function."""
         return
 
     monkeypatch.setattr(Account, "find_by_email", mock_find_by_email)
     monkeypatch.setattr(Account, "update", mock_update)
 
-    token = Token.generate_token({'email': EMAIL, 'campus_id': CAMPUS_ID})
-    response = client.put('/update-authorization', headers={'Authorization': f'Bearer {token}'}, json={
-                          'app_id': 'test-app', 'authorization': {'access': True}})
+    token = Token.generate_token(ADMIN_ACCOUNT)
+    response = client.put('/update-authorization',
+                          headers={'Authorization': f'Bearer {token}'},
+                          json={
+                              'email': 'user@ncsu.edu',
+                              'app_id': 'test-app',
+                              'authorization': {'access': True}
+                          })
+
     assert response.status_code == 200
     assert response.json() == {
         'authorizations': {
@@ -32,18 +129,87 @@ def test_update_authorization(monkeypatch):
     }
 
 
-def test_delete_authorization(monkeypatch):
-    def mock_find_by_email(*args, **kwargs):
-        return Account({'id': '62bc911d2947f5aa76600598', 'email': EMAIL, 'campus_id': CAMPUS_ID, 'authorizations': {'test-app': {'access': True}}})
+def test_update_authorization_without_permission(monkeypatch):
+    """
+    It should fail to update a user's authorization if the requester
+    does not have permission.
+    """
 
-    def mock_update(*args, **kwargs):
+    def mock_find_by_email(email):
+        """Mock Account.find_by_email function."""
+        if email == 'admin@ncsu.edu':
+            return Account(ADMIN_ACCOUNT_WITHOUT_PERMISSION)
+        else:
+            return Account(ACCOUNT_TO_BE_CHANGED)
+
+    def mock_update(*_, **__):
+        """Mock Account.update function."""
         return
 
     monkeypatch.setattr(Account, "find_by_email", mock_find_by_email)
     monkeypatch.setattr(Account, "update", mock_update)
 
-    token = Token.generate_token({'email': EMAIL, 'campus_id': CAMPUS_ID})
+    token = Token.generate_token(ADMIN_ACCOUNT)
+    response = client.put('/update-authorization',
+                          headers={'Authorization': f'Bearer {token}'},
+                          json={
+                              'email': 'user@ncsu.edu',
+                              'app_id': 'test-app',
+                              'authorization': {'access': True}
+                          })
+
+    assert response.status_code == 400
+
+
+def test_delete_authorization(monkeypatch):
+    """It should ensure a user's authorization can be deleted."""
+
+    def mock_find_by_email(email):
+        """Mock Account.find_by_email function."""
+        if email == 'admin@ncsu.edu':
+            return Account(ADMIN_ACCOUNT)
+        else:
+            return Account(ACCOUNT_TO_BE_CHANGED)
+
+    def mock_update(*_, **__):
+        """Mock Account.update function."""
+        return
+
+    monkeypatch.setattr(Account, "find_by_email", mock_find_by_email)
+    monkeypatch.setattr(Account, "update", mock_update)
+
+    token = Token.generate_token(ADMIN_ACCOUNT)
     response = client.delete(
-        '/delete-authorization/test-app', headers={'Authorization': f'Bearer {token}'})
+        '/delete-authorization?app_id=test-app&email=user@ncsu.edu',
+        headers={'Authorization': f'Bearer {token}'})
+
     assert response.status_code == 200
     assert response.json() == {'authorizations': {}}
+
+
+def test_delete_authorization_without_permission(monkeypatch):
+    """
+    It should fail to delete a user's authorizations if the requester
+    does not have permission.
+    """
+    
+    def mock_find_by_email(email):
+        """Mock Account.find_by_email function."""
+        if email == 'admin@ncsu.edu':
+            return Account(ADMIN_ACCOUNT_WITHOUT_PERMISSION)
+        else:
+            return Account(ACCOUNT_TO_BE_CHANGED)
+
+    def mock_update(*_, **__):
+        """Mock Account.update function."""
+        return
+
+    monkeypatch.setattr(Account, "find_by_email", mock_find_by_email)
+    monkeypatch.setattr(Account, "update", mock_update)
+
+    token = Token.generate_token(ADMIN_ACCOUNT)
+    response = client.delete(
+        '/delete-authorization?app_id=test-app&email=user@ncsu.edu',
+        headers={'Authorization': f'Bearer {token}'})
+
+    assert response.status_code == 400
